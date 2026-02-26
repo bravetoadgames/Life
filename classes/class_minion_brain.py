07e012e89cd98ae3d280bf512e7937c351f37a6d
Arjen Schumacher <arjen.schumacher@gmail.com>
2026-02-26 08:41:17 +0100

    Added skeleton class.

--- classes/class_minion_brain.py
+++ classes/class_minion_brain.py
@@ -2,5 +2,22 @@
 
 class Brain:
     
+
     def __init__(self):
+        self.dopamine = 100
+        self.melatonine = 100
+        self.serotonin = 100
+        
+
+
+    def setDopamine(self):
         pass
+
+
+    def setMelatonine(self):
+        pass
+
+
+    def setSerotonin(self):
+        pass
+    
